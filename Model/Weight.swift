bfc02ad1eea5c403c3df4eb6364205ee45676f43
Yohai Reshef <Yohai.Reshef@gmail.com>
2020-05-29 11:53:17 +0300

    initial commit

---
+++ Model/Weight.swift
@@ -0,0 +1,14 @@
+//
+//  Weight.swift
+//  WeightTracker
+//
+//  Created by Yohai Reshef on 29/05/2020.
+//  Copyright © 2020 Yohai Reshef. All rights reserved.
+//
+
+import UIKit
+
+struct Weight {
+    
+    private(set) var weight: Int
+}
