a0a4265de563f2e5870b962631658f1f86a5aebe
Yohai Reshef <Yohai.Reshef@gmail.com>
2020-05-29 11:01:51 +0300

    Initial Commit

---
+++ WeightTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightTracker
+//
+//  Created by Yohai Reshef on 29/05/2020.
+//  Copyright © 2020 Yohai Reshef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
